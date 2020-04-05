c8e980d9e982322e461381e3efb09cada01f400f
Nedimar Paulo Turatti <npturatti@gmail.com>
2020-04-05 12:49:26 -0300

    [app] Shows all articles from DB.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.web_console.whitelisted_ips = ['172.16.0.0/12', '192.168.0.0/16']    
   end
 end
